627a8c37c5b2fe0567f26447d051a06b46ed2477
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-12-23 01:08:40 -0500

    Se crea un formulario con validacion

--- blocks/registros/registroEmplazamiento/script/ready.php
+++ blocks/registros/registroEmplazamiento/script/ready.php
@@ -1,3 +1,26 @@
+
+<?php
+$nombrePagina = $this->miConfigurador->getVariableConfiguracion ( 'pagina' );
+$nombreBloque = $this->miConfigurador->getVariableConfiguracion ( 'esteBloque' )['nombre'];
+?>
+
+$("#<?php echo $nombreBloque.'Registrar';?>").validationEngine({
+promptPosition : "bottomRight:-150",
+scroll: false,
+autoHidePrompt: true,
+autoHideDelay: 2000
+});
+
+$(function() {
+	$("#<?php echo $nombreBloque.'Registrar';?>").submit(function() {
+		$resultado=$("#novedadesBonificacion").validationEngine("validate");
+		if ($resultado) {
+			return true;
+		}
+		return false;
+	});
+});
+
 // Asociar el widget de validación al formulario
 
 /////////Se define el ancho de los campos de listas desplegables///////
