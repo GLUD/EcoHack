cdc756ae775638a3956bcb465c5fa0c818193a2c
Jennifer Pacheco <jvivian14@hotmail.com>
2016-01-14 14:52:21 -0500

    campo foto con codificacion base 64

--- blocks/registros/registroEmplazamiento/funcion/RegistrarEmplazamiento.php
+++ blocks/registros/registroEmplazamiento/funcion/RegistrarEmplazamiento.php
@@ -27,10 +27,22 @@ class Registrar {
 		$this->miFuncion = $funcion;
 	}
 	function procesarFormulario() {
+	foreach ( $_FILES as $key => $values ) { $archivo = $_FILES [$key]; }
+	$imagedata = file_get_contents($archivo["tmp_name"]);
+	$base64= base64_encode($imagedata);
+	//reconstruir una imagen desde el codigo base 64
+	//echo '<img  src="data:image/jpeg;base64,'.$base64.'" />';
+	
+	
+		//var_dump($_REQUEST);   var_dump ($_FILES);  var_dump($archivo);die;
+	
 
 		$conexion = "modelo";
 		$esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
 		$_REQUEST['id_usuario'] = '3';
+		$_REQUEST['imagen'] = $base64;
+		
+		
 		$cadenaSql = $this->miSql->getCadenaSql ( 'registrarEmplazamiento', $_REQUEST );
 		
 		$resultado = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "insertar" );
